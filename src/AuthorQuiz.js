f5a3354dfe8c17964f1c3e6ec505f5a50ec5fc4c
Perez Hernandez, M <m.perez.hernandez@accenture.com>
2020-03-29 22:59:07 -0600

    next, chapter 5

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,13 +1,67 @@
+//Llibraries:
 import React from 'react';
+//Components:
 import './AuthorQuiz.css';
-import './bootstrap.min.css'
+//Bootstrap:
+import './bootstrap.min.css';
 
-function AuthorQuiz() {
+function Hero(){
   return (
-    <div className="AuthorQuiz">
-      <header className="AuthorQuiz-header">
-        <p><code>authorquiz</code></p>
-      </header>
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1> Author Quiz</h1>
+        <p>To play, select the book written by the author shown in the picture</p>
+      </div>
+    </div>
+  );
+}
+
+function Book({title}){
+  return(
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  )
+}
+
+function Turn({author,books}){
+  return(
+    <div className="row turn" style={{backgroundColor: "white"}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt={author.name} />
+      </div>
+      <div className="col-6">
+        {books.map(
+          (title) => <Book title={title} key={title} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Continue(){
+  return(
+    <div></div>
+  );
+}
+
+function Footer(){
+  return(
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">all images used from <a href="http://commons.wikimedia.org">Wikimedia Commons</a> and are in the public domain</p>
+      </div>
+    </div>
+  );
+}
+
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid">
+      <Hero/>
+      <Turn {...turnData}/>
+      <Continue/>
+      <Footer/>
     </div>
   );
 }
